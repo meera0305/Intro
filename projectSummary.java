7ce6c44ae8feebdf9ca8f0c3779b44341d3e4f00
meera0305 <chavanmeera66@gmail.com>
2025-06-21 13:35:20 +0530

    Create projectSummary.java

---
+++ projectSummary.java
@@ -0,0 +1,22 @@
+public class ProjectSummary {
+    public static void showSummary() {
+        System.out.println("==============================================");
+        System.out.println("         📚 Project Summary – intro");
+        System.out.println("==============================================");
+        System.out.println(" Project Title : intro");
+        System.out.println(" Developer     : Meera Chavan");
+        System.out.println(" Technologies  : Java, JDBC, MySQL");
+        System.out.println(" Project Type  : Console-based CRUD App");
+        System.out.println(" Modules       :");
+        System.out.println("   ✔ ");
+        System.out.println("   ✔");
+        System.out.println("   ");
+        System.out.println("----------------------------------------------");
+        System.out.println(" Description:");
+        System.out.println(" ➤ A simple Java application for mananging my Data");
+        System.out.println("    and course data using database connectivity.");
+        System.out.println(" ➤ It demonstrates real-time CRUD operations.");
+        System.out.println(" ➤ Clean modular code using OOP concepts.");
+        System.out.println("==============================================\n");
+    }
+}
